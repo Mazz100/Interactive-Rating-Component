1c6ef0846d74aa6a872ce3b58a7d3fdf7749caef
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-01-04 22:03:55 +0200

    Radio Form Validation Update #2
    - Simplified the validation by using built-in property to FormData.
    
    - Add/Remove classes for UI visibility.

--- jscode.js
+++ jscode.js
@@ -1,26 +1,24 @@
 const form = document.querySelector('#form');
-const input = document.querySelector('rating');
 const rateValue = document.querySelector('#rate');
+const successUI = document.querySelector('.UI-2-wrapper');
+const rateUI = document.querySelector('.UI-1-wrapper');
 //Get input data when pressing on a submit button
 form.addEventListener("submit", (submission) => {
     submission.preventDefault();
+    const formdata = new FormData(form);
+    //Getting the input name property shared between all inputs
+    const rating = formdata.get('rating');
 
-    const data = new FormData(form);
-    let valid = false;
-    let output = "";
-
-    for (const entry of data) {
-        output = `${output}${entry[1]}\r`;
-    }
-
-    if (output != "") {
-        valid = true;
-        console.log(output);
-        rateValue.textContent = output;
+    if (!rating) {
+        console.log('Please choose a rate!');
     }
     else {
-        console.log('Please choose a rate!');
-        return false;
+        //Add rating option to the text
+        rateValue.textContent = rating;
+
+        //Show success screen and hide Rate screen
+        rateUI.classList.add('UI-1-visibility');
+        successUI.classList.remove('UI-2-visibility');
 
     }
 
