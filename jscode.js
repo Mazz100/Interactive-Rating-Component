5ed91ca3b4c4ddaaf0d59af21ae57fc1edc1b35b
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-01-05 20:17:02 +0200

    Final tweaks to design

--- jscode.js
+++ jscode.js
@@ -1,26 +1,20 @@
 const form = document.querySelector('#form');
 const rateValue = document.querySelector('#rate');
-const successUI = document.querySelector('.UI-2-wrapper');
 const rateUI = document.querySelector('.UI-1-wrapper');
+const successUI = document.querySelector('.UI-2-wrapper');
+
 //Get input data when pressing on a submit button
 form.addEventListener("submit", (submission) => {
     submission.preventDefault();
     const formdata = new FormData(form);
-    //Getting the input name property shared between all inputs
+    //Getting the input name attribute shared between all inputs
     const rating = formdata.get('rating');
 
-    if (!rating) {
-        console.log('Please choose a rate!');
-    }
-    else {
-        //Add rating option to the text
-        rateValue.textContent = rating;
+    //Add rating option to the text
+    rateValue.textContent = rating;
 
-        //Show success screen and hide Rate screen
-        rateUI.classList.add('UI-1-visibility');
-        successUI.classList.remove('UI-2-visibility');
-
-    }
-
+    //Show success screen and hide Rate screen
+    rateUI.classList.add('UI-1-visibility');
+    successUI.classList.remove('UI-2-visibility');
 
 })
